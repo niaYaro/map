8757d647a3053cbe5a4f93ab825ec3c114050234
niaYaro <111086548+niaYaro@users.noreply.github.com>
2022-08-16 17:24:59 +0300

    Add files via upload

---
+++ Europe map/js/skript.js
@@ -0,0 +1,264 @@
+function showFinFlag() {
+    document.getElementById("info-fin").style.display = "block";
+}
+function hideFinFlag() {
+    document.getElementById("info-fin").style.display = "none";
+}
+function showSwedFlag() {
+    document.getElementById("info-swed").style.display = "block";
+}
+function hideSwedFlag() {
+    document.getElementById("info-swed").style.display = "none";
+}
+function showAlbFlag() {
+    document.getElementById("info-alb").style.display = "block";
+}
+function hideAlbFlag() {
+    document.getElementById("info-alb").style.display = "none";
+}
+function showAndFlag() {
+    document.getElementById("info-and").style.display = "block";
+}
+function hideAndFlag() {
+    document.getElementById("info-and").style.display = "none";
+}
+function showArmFlag() {
+    document.getElementById("info-arm").style.display = "block";
+}
+function hideArmFlag() {
+    document.getElementById("info-arm").style.display = "none";
+}
+function showAusFlag() {
+    document.getElementById("info-aus").style.display = "block";
+}
+function hideAusFlag() {
+    document.getElementById("info-aus").style.display = "none";
+}
+function showBelFlag() {
+    document.getElementById("info-bel").style.display = "block";
+}
+function hideBelFlag() {
+    document.getElementById("info-bel").style.display = "none";
+}
+function showBulFlag() {
+    document.getElementById("info-bul").style.display = "block";
+}
+function hideBulFlag() {
+    document.getElementById("info-bul").style.display = "none";
+}
+function showBahFlag() {
+    document.getElementById("info-bah").style.display = "block";
+}
+function hideBahFlag() {
+    document.getElementById("info-bah").style.display = "none";
+}
+function showBerFlag() {
+    document.getElementById("info-ber").style.display = "block";
+}
+function hideBerFlag() {
+    document.getElementById("info-ber").style.display = "none";
+}
+function showSwiFlag() {
+    document.getElementById("info-swi").style.display = "block";
+}
+function hideSwiFlag() {
+    document.getElementById("info-swi").style.display = "none";
+}
+function showCzeFlag() {
+    document.getElementById("info-cze").style.display = "block";
+}
+function hideCzeFlag() {
+    document.getElementById("info-cze").style.display = "none";
+}
+function showCypFlag() {
+    document.getElementById("info-cyp").style.display = "block";
+}
+function hideCypFlag() {
+    document.getElementById("info-cyp").style.display = "none";
+}
+function showGerFlag() {
+    document.getElementById("info-ger").style.display = "block";
+}
+function hideGerFlag() {
+    document.getElementById("info-ger").style.display = "none";
+}
+function showDenFlag() {
+    document.getElementById("info-den").style.display = "block";
+}
+function hideDenFlag() {
+    document.getElementById("info-den").style.display = "none";
+}
+function showEstFlag() {
+    document.getElementById("info-est").style.display = "block";
+}
+function hideEstFlag() {
+    document.getElementById("info-est").style.display = "none";
+}
+function showUnkFlag() {
+    document.getElementById("info-unk").style.display = "block";
+}
+function hideUnkFlag() {
+    document.getElementById("info-unk").style.display = "none";
+}
+function showGeoFlag() {
+    document.getElementById("info-geo").style.display = "block";
+}
+function hideGeoFlag() {
+    document.getElementById("info-geo").style.display = "none";
+}
+function showGreFlag() {
+    document.getElementById("info-gre").style.display = "block";
+}
+function hideGreFlag() {
+    document.getElementById("info-gre").style.display = "none";
+}
+function showCroFlag() {
+    document.getElementById("info-cro").style.display = "block";
+}
+function hideCroFlag() {
+    document.getElementById("info-cro").style.display = "none";
+}
+function showHunFlag() {
+    document.getElementById("info-hun").style.display = "block";
+}
+function hideHunFlag() {
+    document.getElementById("info-hun").style.display = "none";
+}
+function showIrlFlag() {
+    document.getElementById("info-irl").style.display = "block";
+}
+function hideIrlFlag() {
+    document.getElementById("info-irl").style.display = "none";
+}
+function showIceFlag() {
+    document.getElementById("info-ice").style.display = "block";
+}
+function hideIceFlag() {
+    document.getElementById("info-ice").style.display = "none";
+}
+function showItaFlag() {
+    document.getElementById("info-ita").style.display = "block";
+}
+function hideItaFlag() {
+    document.getElementById("info-ita").style.display = "none";
+}
+function showLchFlag() {
+    document.getElementById("info-lch").style.display = "block";
+}
+function hideLchFlag() {
+    document.getElementById("info-lch").style.display = "none";
+}
+function showLthFlag() {
+    document.getElementById("info-lth").style.display = "block";
+}
+function hideLthFlag() {
+    document.getElementById("info-lth").style.display = "none";
+}
+function showLuxFlag() {
+    document.getElementById("info-lux").style.display = "block";
+}
+function hideLuxFlag() {
+    document.getElementById("info-lux").style.display = "none";
+}
+function showLatFlag() {
+    document.getElementById("info-lat").style.display = "block";
+}
+function hideLatFlag() {
+    document.getElementById("info-lat").style.display = "none";
+}
+function showMolFlag() {
+    document.getElementById("info-mol").style.display = "block";
+}
+function hideMolFlag() {
+    document.getElementById("info-mol").style.display = "none";
+}
+function showNmcFlag() {
+    document.getElementById("info-nmc").style.display = "block";
+}
+function hideNmcFlag() {
+    document.getElementById("info-nmc").style.display = "none";
+}
+function showMonFlag() {
+    document.getElementById("info-mon").style.display = "block";
+}
+function hideMonFlag() {
+    document.getElementById("info-mon").style.display = "none";
+}
+function showNorFlag() {
+    document.getElementById("info-nor").style.display = "block";
+}
+function hideNorFlag() {
+    document.getElementById("info-nor").style.display = "none";
+}
+function showPolFlag() {
+    document.getElementById("info-pol").style.display = "block";
+}
+function hidePolFlag() {
+    document.getElementById("info-pol").style.display = "none";
+}
+function showPorFlag() {
+    document.getElementById("info-por").style.display = "block";
+}
+function hidePorFlag() {
+    document.getElementById("info-por").style.display = "none";
+}
+function showRomFlag() {
+    document.getElementById("info-rom").style.display = "block";
+}
+function hideRomFlag() {
+    document.getElementById("info-rom").style.display = "none";
+}
+function showSrbFlag() {
+    document.getElementById("info-srb").style.display = "block";
+}
+function hideSrbFlag() {
+    document.getElementById("info-srb").style.display = "none";
+}
+function showSlkFlag() {
+    document.getElementById("info-slk").style.display = "block";
+}
+function hideSlkFlag() {
+    document.getElementById("info-slk").style.display = "none";
+}
+function showSlnFlag() {
+    document.getElementById("info-sln").style.display = "block";
+}
+function hideSlnFlag() {
+    document.getElementById("info-sln").style.display = "none";
+}
+function showTurFlag() {
+    document.getElementById("info-tur").style.display = "block";
+}
+function hideTurFlag() {
+    document.getElementById("info-tur").style.display = "none";
+}
+function showUkrFlag() {
+    document.getElementById("info-ukr").style.display = "block";
+}
+function hideUkrFlag() {
+    document.getElementById("info-ukr").style.display = "none";
+}
+function showKosFlag() {
+    document.getElementById("info-kos").style.display = "block";
+}
+function hideKosFlag() {
+    document.getElementById("info-kos").style.display = "none";
+}
+function showNthFlag() {
+    document.getElementById("info-nth").style.display = "block";
+}
+function hideNthFlag() {
+    document.getElementById("info-nth").style.display = "none";
+}
+function showSpaFlag() {
+    document.getElementById("info-spa").style.display = "block";
+}
+function hideSpaFlag() {
+    document.getElementById("info-spa").style.display = "none";
+}
+function showFraFlag() {
+    document.getElementById("info-fra").style.display = "block";
+}
+function hideFraFlag() {
+    document.getElementById("info-fra").style.display = "none";
+}
